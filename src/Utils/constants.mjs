412ce07ffe14a8154a007c483d523b517a24503f
Billy Kaufman <billy.kaufman@concertiv.com>
2024-02-15 18:55:01 -0500

    Tag storage keys with project name

--- src/Utils/constants.mjs
+++ src/Utils/constants.mjs
@@ -1,4 +1,4 @@
-export const CROPPED_IMAGE_STORAGE_KEY = "croppedImage";
-export const SCREENSHOT_IMAGE_STORAGE_KEY = "screenshot";
-export const SCREENSHOT_URL_STORAGE_KEY = "screenshotUrl";
 export const PROJECT_NAME = "PageNote";
+export const CROPPED_IMAGE_STORAGE_KEY = `${PROJECT_NAME}_croppedImage`;
+export const SCREENSHOT_IMAGE_STORAGE_KEY = `${PROJECT_NAME}_screenshot`;
+export const SCREENSHOT_URL_STORAGE_KEY = `${PROJECT_NAME}_screenshotUrl`;
